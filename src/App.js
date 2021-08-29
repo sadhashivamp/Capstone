b7e38600c352104d9f93fbf16f11b3dd3d43d83f
sadhashivamp <ramulu320p@gmail.com>
2021-08-29 12:41:59 +0530

    added logo

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { User } from './components/User';
-// import { SignUp } from './components/SignUp';
-import './App.css'
 import { Builder } from './components/Builder';
 import { SignUp } from './components/SignUp';
 import { Home } from './components/Home';
-// import { ShowCase } from './components/ShowCase';
+import './App.css'
+
+import { ShowCase } from './components/ShowCase';
 
 const App = () => {
     return (
         <div>
             
                 <Router>
-                    
+                
                         <Header />
-                        {/* <ShowCase /> */}
+                        
+                    <Route exact path='/home'>
+                        <ShowCase />
+                    </Route>    
                     <Route exact path='/'>
                         <Home />
                     </Route>
@@ -38,7 +41,7 @@ const App = () => {
         
                 </Router>
             
-           
+                
         </div>
     )
 }
