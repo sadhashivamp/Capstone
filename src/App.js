975d6a0db54bf86acabd917872a65b21c4e3b818
saurabh parasar <saurabhparasar3010@gmail.com>
2021-08-31 10:05:00 +0530

    user

--- src/App.js
+++ src/App.js
@@ -1,48 +1,35 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Header } from './components/Header';
-import { User } from './components/User';
-import { Builder } from './components/Builder';
-import { SignUp } from './components/SignUp';
-import { Home } from './components/Home';
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Notifications from "./pages/Notifications";
+import Projects from "./pages/Projects";
+import Post from "./pages/Post";
+import Chats from "./pages/Chats";
+import UserProfile from "./pages/UserProfile";
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import { Header } from './components/Header';
+// import { User } from './components/User';
+// import { Builder } from './components/Builder';
+// import { SignUp } from './components/SignUp';
+// import { Home } from './components/Home';
+import "./App.css";
 
-import { ShowCase } from './components/ShowCase';
+// import { ShowCase } from './components/ShowCase';
 
 const App = () => {
-    return (
-        <div>
-            
-                <Router>
-                
-                        <Header />
-                        
-                    <Route exact path='/home'>
-                        <ShowCase />
-                    </Route>    
-                    <Route exact path='/'>
-                        <Home />
-                    </Route>
-                  
-                    <Route path='/user'>
-                        <User />
-                    </Route>
-
-                    <Route path='/builder'>
-                        <Builder />
-                    </Route>
-
-                    <Route path='/signup'>
-                        <SignUp />
-                    </Route>
-                    
-                       
-                    
-        
-                </Router>
-            
-                
-        </div>
-    )
-}
-export default App
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={UserProfile} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/post" component={Post} />
+          <Route path="/notifications" component={Notifications} />
+          <Route path="/chats" component={Chats} />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+export default App;
