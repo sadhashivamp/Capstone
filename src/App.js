0a80b74ab9f2ede85a7108dd65787ecad80635e8
sadhashivamp <ramulu320p@gmail.com>
2021-09-12 14:25:21 +0530

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -65,68 +64,4 @@ const App = () => {
   );
 };
 
-=======
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import Notifications from "./user/Notifications";
-import Projects from "./user/Projects";
-import Post from "./user/Post";
-import Chats from "./user/Chats";
-import UserProfile from "./user/UserProfile";
-import UserDashboard from "./components/UserDashboard";
-// import { Header } from './components/Header';
-// import { User } from './components/User';
-// import { Builder } from './components/Builder';
-// import { SignUp } from './components/SignUp';
-// import { Home } from './components/Home';
-import "./App.css";
-// import BuilderDashboard from "./components/BuilderDashboard";
-// import Profile from "./builder/Profile";
-// import Notifications from "./builder/Notifications";
-// import Available from "./builder/Available";
-// import Chats from "./builder/Chats";
-// import MyBids from "./builder/MyBids";
-
-// import { ShowCase } from './components/ShowCase';
-import Card from "./components/Card.jsx";
-
-const App = () => {
-  return (
-    <>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </>
-  );
-  // return (
-  //   <div>
-  //     <Router>
-  //       <UserDashboard />
-  //       <Switch>
-  //         <Route path="/" exact component={UserProfile} />
-  //         <Route path="/projects" component={Projects} />
-  //         <Route path="/post" component={Post} />
-  //         <Route path="/notifications" component={Notifications} />
-  //         <Route path="/chats" component={Chats} />
-  //       </Switch>
-  //     </Router>
-  //   </div>
-  // );
-  // return (
-  //   <div>
-  //     <Router>
-  //       <BuilderDashboard />
-  //       <Switch>
-  //         <Route path="/" exact component={Profile} />
-  //         <Route path="/projects" component={MyBids} />
-  //         <Route path="/post" component={Available} />
-  //         <Route path="/notifications" component={Notifications} />
-  //         <Route path="/chats" component={Chats} />
-  //       </Switch>
-  //     </Router>
-  //   </div>
-  // );
-};
->>>>>>> a60b16c8bcbf9e2fcd5d48e76d1ea8aaecf89797
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,14 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
->>>>>>> a60b16c8bcbf9e2fcd5d48e76d1ea8aaecf89797
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
